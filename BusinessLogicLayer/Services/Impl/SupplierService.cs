0f5d2aa4e87d080a39ec2fa677869def8ab610a8
DimaLemeshuk <dmutrolemeshuk@gmail.com>
2023-12-04 19:15:54 +0200

    Added ModelServices

---
+++ BusinessLogicLayer/Services/Impl/SupplierService.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLogicLayer.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services.Impl
+{
+    public class SupplierService : ISupplierService
+    {
+    }
+}
